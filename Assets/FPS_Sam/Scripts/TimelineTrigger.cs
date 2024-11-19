d6e6f4773ac6736770c23fa2541d91c2acbc6ec7
DentedGlassJar <nathanielwhite867991@gmail.com>
2024-11-19 12:24:28 -0400

    Project Begun

---
+++ Assets/FPS_Sam/Scripts/TimelineTrigger.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimelineTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
